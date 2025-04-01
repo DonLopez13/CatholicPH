72c38a5fd7f1ffe38be662811b4c33cad1fd6617
DonLopez13 <donvitolopez7@gmail.com>
2025-04-01 18:02:18 +0800

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Placeholder for future JavaScript functionality
-
-// Example: You can add a function here to handle form submissions, dynamic content, etc.
-console.log("Scripts loaded successfully!");
